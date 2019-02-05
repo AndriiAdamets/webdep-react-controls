4dfa6d3418b4a3dd5a7a706e5de1996e44a74be1
Andrii Adamets <andrey.adamets@gmail.com>
2019-02-05 15:54:52 +0200

    Add tooltip component

---
+++ src/lib/components/Tooltip/Tooltip.md
@@ -0,0 +1,6 @@
+```js
+<div><Tooltip body="Tooltip">Top</Tooltip></div>
+<div><Tooltip body="Tooltip" position="right">Right</Tooltip></div>
+<div><Tooltip body="Tooltip" position="bottom">Bottom</Tooltip></div>
+<div><Tooltip body="Tooltip" position="left">Left</Tooltip></div>
+```
---
+++ src/lib/components/Tooltip/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createPortal, } from 'react-dom';
+import classnames from 'classnames';
+
+import getPortalContainer from '../../helpers/getPortalContainer';
+/**
+ * Component add tooltip to its children on hover
+ */
+export default class Tooltip extends Component {
+  // TODO: Make tooltip visible from props; Add tests for components with createPortal
+  static propTypes = {
+    /** Is tooltip on top or on bottom from target */
+    position: PropTypes.oneOf(['top', 'bottom', 'right', 'left']),
+    /** Tooltip background color */
+    state: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']),
+    /** Tooltip message */
+    body: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    bodyStyle: PropTypes.object,
+  };
+
+  static defaultProps = {
+    position: 'top',
+    state: 'dark',
+  };
+
+  state = {
+    isTooltipVisible: false
+  };
+
+  get bodyPosition() {
+    if (!this.trigger) {
+      return {};
+    }
+    const { position } = this.props;
+    const rect = this.trigger.getBoundingClientRect();
+    let left, top;
+    if (position === 'top') {
+      left = rect.left + rect.width / 2;
+      top = window.scrollY + rect.top;
+    }
+    if (position === 'bottom') {
+      left = rect.left + rect.width / 2;
+      top = window.scrollY + rect.top + rect.height;
+    }
+    if (position === 'right') {
+      left = rect.left + rect.width;
+      top = window.scrollY + rect.top + rect.height;
+    }
+    if (position === 'left') {
+      left = rect.left;
+      top = window.scrollY + rect.top + rect.height;
+    }
+    return { left, top };
+  }
+
+  get content() {
+    const { body } = this.props;
+    if (typeof body === 'string') {
+      return (
+        <div dangerouslySetInnerHTML={{ __html: body }}></div>
+      );
+    }
+    return body;
+  }
+
+  get body() {
+    const { body, state, position, bodyStyle } = this.props;
+    const { isTooltipVisible } = this.state;
+
+    if (!!body && (!!isTooltipVisible)) {
+      return createPortal(
+        (<div ref={(div) => this.tooltipBody = div}
+          role="tooltip"
+          className={classnames(
+            'wrc-tooltip__body',
+            [`wrc-tooltip__body--${state}`],
+            [`wrc-tooltip__body--${position}`]
+          )}
+          style={{ ...this.bodyPosition, ...bodyStyle }}>
+          {this.content}
+        </div >),
+        getPortalContainer()
+      );
+    }
+    return null;
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ isTooltipVisible: true });
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ isTooltipVisible: false });
+  }
+
+  render() {
+    const { children, className, style, visible } = this.props;
+    return (
+      <div className={classnames('wrc-tooltip', className)} style={style}
+        ref={div => this.trigger = div}
+        onMouseEnter={(visible === undefined) ? this.handleMouseEnter : undefined}
+        onMouseLeave={(visible === undefined) ? this.handleMouseLeave : undefined}>
+        {children}
+        {this.body}
+      </div>
+    );
+  }
+}
+
