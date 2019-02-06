3a7e4cbc05ff1eb5cb5dda8586f3866ded2decfd
Andrii Adamets <andrey.adamets@gmail.com>
2019-02-06 15:59:55 +0200

    Add input component

---
+++ src/lib/components/Input/index.js
@@ -0,0 +1,10 @@
+import React, { forwardRef } from 'react';
+import classnames from 'classnames';
+const Input = ({ className, invalid, formControl, ...inputProps }, ref) => (
+  <input {...inputProps} className={classnames('wrc-input', {
+    'wrc-input--form-control': !!formControl,
+    'wrc-input--invalid': !!invalid
+  })} ref={ref} />
+);
+
+export default forwardRef(Input);
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,5 @@ import Button from './components/Button';
 import Card from './components/Card';
 import Tooltip from './components/Tooltip';
 import Checkbox from './components/Checkbox';
-export { Button, Card, Tooltip, Checkbox, };
+import Input from './components/Input';
+export { Button, Card, Tooltip, Checkbox, Input };
